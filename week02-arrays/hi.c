78166ae88bcb462a2c2c480791cdfdfb946bd022
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-01-25 21:35:15 -0300

    .

--- week02-arrays/hi.c
+++ week02-arrays/hi.c
@@ -1,25 +1,19 @@
 # include<stdio.h>
-
+# include <cs50.h> 
+# include <string.h> 
 int main (void){
-	// Manual String input	
-	int size; 
-	printf("Size of the word: ");
-	scanf("%d", &size);
 
-	char string[size]; 
-	for(int i = 0; i < size; i ++){
-		printf("%do word : ", i+1); 	
-		getchar(); 
-		string[i] = getchar();
+	// double array of strings
+	// Arrays of strings -> arrays of arrays 
+	string words[2]; 
+	words[0] = "HI!"; 
+	words[1] = "BYE!"; // matrix
+	
+	int length = sizeof(words) / sizeof(words[0]); 
+	for( int i = 0; i < length; i ++){   
+		for (int j = 0; j < strlen(words[i]); j++){ 
+			printf("%c", words[i][j]); 	
+		}
+		printf("\n"); 
 	}
-	// I'm printing not a String but a char left to right 	
-	for(int i = 0; i < size ; i++){
-		printf("%i", string[i]); 
-		// Here it will print the numeric representation for each character in ASCII.  
-	} 
-	// But it is here an array anyway. 
-	// in this way to do an array, it will not have the \0 at the end. 
-
-	printf("\n");
 }
-
