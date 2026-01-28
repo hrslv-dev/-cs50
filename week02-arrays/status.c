0db5048631b3257437944b6404116adbe21b82ca
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-01-28 20:15:22 -0300

    Correcting a bug in the error. The code stills execute even whit the error

--- week02-arrays/status.c
+++ week02-arrays/status.c
@@ -11,26 +11,28 @@ int main(int argc, string argv[]){
     if (typed_number_error(argc, argv) != 0){ 
         return -1; 
     } 
+
     printf("Hello my boy, %s\n",argv[1]); 
     return 0; 
 }
 // I can't type numbers in this 
 int typed_number_error(int argc, string argv[]){ 
-    if (invalid_number_of_arguments(argc, argv) == 0) {
-        string str = argv[1]; 
-        for(int i = 0, n = strlen(str); i < n; i++){
-            if (isdigit(str[i])){
-                printf("TYPE_DIGIT_ERROR\n"); 
-                return -1; 
-            } 
-        }
-     }
+    if (invalid_number_of_arguments(argc, argv) != 0) {
+        return -1;
+    }
+    string str = argv[1]; 
+    for(int i = 0, n = strlen(str); i < n; i++){
+        if (isdigit(str[i])){
+            printf("TYPE_DIGIT_ERROR\n"); 
+            return -1; 
+        } 
+    }
     return 0; 
  }
 
  int invalid_number_of_arguments(int argc, string argv[]){ 
     if ( argc != 2 ){
-        printf("INVALID NUMEBR OF ARGUMENTS ERROR\n"); 
+        printf("INVALID NUMBER OF ARGUMENTS ERROR\n"); 
         return -1; 
      } 
     return 0; 
