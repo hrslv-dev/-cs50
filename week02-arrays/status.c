bfa324673d5b7fe28af7fe27d559c3a11894c587
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-01-28 21:10:02 -0300

    study day 01-28-2026

--- week02-arrays/status.c
+++ week02-arrays/status.c
@@ -3,13 +3,15 @@
 # include <string.h> 
 # include <ctype.h>
 
+// THIS CODE IS SUPPOSED TO TEST SOME GENERAL ERROS AND LEARN MORE ABOUT: 
+// EXIT STATUS, COMMAND LINE ARGUMENTS and ERRORS. 
+
 int invalid_number_of_arguments(int argc, string argv[]); 
 int typed_number_error(int argc, string argv[]); 
 int main(int argc, string argv[]){ 
-
-// I can use this to make exceptions for example like in java
+// FOR EXIT STATUS , THE OS JUST RECEIVES UNSIGNED INTEGERS
     if (typed_number_error(argc, argv) != 0){ 
-        return -1; 
+        return 1; 
     } 
 
     printf("Hello my boy, %s\n",argv[1]); 
@@ -17,14 +19,16 @@ int main(int argc, string argv[]){
 }
 // I can't type numbers in this 
 int typed_number_error(int argc, string argv[]){ 
+    // HERE IS WHERE THE CHANGE IS 
+    // I wasn't making the real valiadtion of the other error
     if (invalid_number_of_arguments(argc, argv) != 0) {
-        return -1;
+        return 1;
     }
     string str = argv[1]; 
     for(int i = 0, n = strlen(str); i < n; i++){
         if (isdigit(str[i])){
             printf("TYPE_DIGIT_ERROR\n"); 
-            return -1; 
+            return 1; 
         } 
     }
     return 0; 
@@ -33,7 +37,7 @@ int typed_number_error(int argc, string argv[]){
  int invalid_number_of_arguments(int argc, string argv[]){ 
     if ( argc != 2 ){
         printf("INVALID NUMBER OF ARGUMENTS ERROR\n"); 
-        return -1; 
+        return 1; 
      } 
     return 0; 
  }
