7dde8ba86b216b9960d1808b5c445f2d601f58a0
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-02-05 19:06:50 -0300

    .

---
+++ week04/adresses.c
@@ -0,0 +1,9 @@
+// how the adresses work? 
+# include <stdio.h> 
+
+int main(void){ 
+    int n = 50; 
+    printf("%i\n", &n); 
+    // This operator shows me the adress in memory of one variable.
+
+}
