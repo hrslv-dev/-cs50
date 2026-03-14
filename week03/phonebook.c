55820e7a7b032d8b9307dee5805ba2b080c4ef63
hrslv-dev <henriqueaugustoss17@gmail.com>
2026-03-14 09:49:28 -0300

    understanding structs

--- week03/phonebook.c
+++ week03/phonebook.c
@@ -1,27 +1,37 @@
-# include <stdio.h>
-# include <cs50.h>
-# include <string.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
 
-int main(void){ 
+typedef struct person createPerson(string name, string number);
+
+int main(void)
+{
 
     // Soulves this with a hasmap
-    string names[] = {"Yullia", "David" ,"John"}; 
-    string numbers[] = {"31-999321509", "31-9954413", "31-9827836"}; 
-    int length_names = sizeof(names) / sizeof(names[0]);
-    int length_numebrs = sizeof(numbers) / sizeof(numbers[0]); 
+    // OR sructs -> like a class with  atributes
+    // Stores variables
 
-    if(length_names != length_numebrs){ 
-        printf("Length doesn't knock!! \n"); 
-        return 1; 
-    }
+    // Same thing as initializing an object
+    person people[3];
+    people[0].name = "David";
+    people[0].number = "+55 (31)99923-1509";
+}
 
-    string name = get_string("Name: "); 
-    // LINEAR SEARCH: 
-    for(int i = 0; i < length_names; i++){ 
-        if(strcmp(names[i], name) == 0 ){ 
-            printf("The correspondent number : %s\n", numbers[i]); 
-            return 0; 
-        }
-    }
-   
+// Typedef i declare explicity that is a alias of a type. 
+
+typedef struct person
+{
+    string number;
+    string name;
+
+} person;
+
+person createPerson(string name, string number)
+{
+    person p;
+
+    strcpy(p.name, name);
+    strcpy(p.number, number);
+
+    return p;
 }
